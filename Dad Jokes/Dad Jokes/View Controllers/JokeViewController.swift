4f193e1178ccb49e8ef5597c2a444b01c7f9b351
Vanna Phong <contact@vanna.io>
2018-07-29 22:34:38 -0700

    initial commit with readme

---
+++ Dad Jokes/Dad Jokes/View Controllers/JokeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JokeViewController.swift
+//  Dad Jokes
+//
+//  Created by Vanna Phong on 29/07/2018.
+//  Copyright © 2018 vphong. All rights reserved.
+//
+
+import UIKit
+
+class JokeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# the dad jokes app
+
+an app that gets a random dad joke from https://icanhazdadjoke.com/graphql and gradient from [uiGradients.com json data](http://uigradients.com/gradients.json)
+
+## roadmap
+
+* grab gradient data
+  * decide whether to save to project itself or populate every time the app is opened
+* connect to dadjoke api
+  * display random dad joke
+  * allow search
+* ui design
+  * copy/paste/share joke
+  * typography design
+  * interaction animations
